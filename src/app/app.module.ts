a1e44d151d45e4ed3cfd40c29b17cb3d547b46c2
kris <krisd3214@gmail.com>
2024-08-16 13:33:29 +0700

    ternyuio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TestComponent } from './test/test.component';
+import { DornComponent } from './dorn/dorn.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TestComponent
+    TestComponent,
+    DornComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/dorn/dorn.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dorn',
+  templateUrl: './dorn.component.html',
+  styleUrls: ['./dorn.component.scss']
+})
+export class DornComponent {
+
+}
